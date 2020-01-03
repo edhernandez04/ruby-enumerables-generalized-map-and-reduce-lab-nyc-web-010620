132d00ba7410dad5b307cd72c247c85960a88a76
Eddie Hernandez <mchh.004@gmail.com>
2020-01-03 15:34:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,3 +8,13 @@ while i < array.length do
 end
 new_array
 end
+
+def reduce(array, start=0)
+  total=0
+  i=0
+  while i < array.length do
+    total += yield(array[i])
+    i+=1
+  end
+  total
+end
